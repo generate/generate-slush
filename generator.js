44993b3e4ab2640ba7b75c9e1e3e3a9cb48deba9
Brian Woodward <brian.woodward@gmail.com>
2016-06-10 15:45:06 -0400

    Usage message when run with `gen slush` Handle mutliple slush generators Better error handling and messages.

--- generator.js
+++ generator.js
@@ -5,38 +5,69 @@ var utils = require('./utils');
 module.exports = function(app) {
   if (!utils.isValid(app, 'generate-slush')) return;
 
-  app.task('slush', function(cb) {
-    console.log(`${app.name} => ${this.name}`);
+  app.task('slush', { silent: true }, function(cb) {
+    var msg = `
+  Usage: ${utils.log.cyan('gen slush.<generator>')}
+
+  generator: part of a slush generator name that comes after the ${utils.log.cyan('`slush-`')} part.
+
+  Examples:
+
+    ${utils.log.gray('# Run the `slush-angular` generator')}
+    ${utils.log.bold('$ gen slush.angular')}
+
+    ${utils.log.gray('# Run the `slush.express` generator')}
+    ${utils.log.bold('$ gen slush.express')}
+
+    `;
+
+    console.log(`${msg}`);
     cb()
   });
 
   app.task('default', { silent: true }, ['slush']);
 
-  // dynamically register sub generator and tasks
-  // based on which slush generator is trying to be run
-
+  // dynamically register sub generators and tasks
+  // based on which slush generators are being run
   var res = utils.parseTasks(app);
-  if (res) {
-    app.register(res.generator, function(sub) {
-      var moduleName = `slush-${res.generator}`;
+
+  if (!res || !res.generators.length) {
+    return;
+  }
+
+  var invoked = {};
+  res.generators.forEach(function(generator) {
+
+    app.register(generator, function(sub) {
+      if (invoked[generator]) return;
+      invoked[generator] = true;
+      var moduleName = `slush-${generator}`;
+      var modulePath = utils.resolveModule(moduleName);
+      if (!modulePath) {
+        console.log();
+        console.log(utils.log.yellow(`  Unable to find "${moduleName}". Make sure it's been installed with npm.`));
+        console.log();
+        return;
+      }
+
       var gulpPath = utils.gulpPath(moduleName);
       if (!gulpPath) {
-        throw new Error('Unable to find a local gulp module');
+        console.log();
+        console.log(utils.log.yellow(`  Unable to find a local gulp module for "${moduleName}"`));
+        console.log();
+        return;
       }
+
       var gulp = require(gulpPath);
       utils.tryRequire(moduleName, 'slushfile.js');
 
       var keys = Object.keys(gulp.tasks || gulp._registry.tasks() || {});
       keys.forEach(function(key) {
-        sub.task(key, utils.task(gulp, key, res.args));
-      });
-
-      // slush generators are expected to handle additional
-      // arguments so this will prevent errors from being
-      // thrown when an argument is used.
-      sub.on('finished', function() {
-        process.exit();
+        sub.task(key, function(cb) {
+          // console.log(`${sub.name} => ${this.name}`);
+          utils.task(gulp, key)(cb);
+        });
       });
     });
-  }
+  });
 };
